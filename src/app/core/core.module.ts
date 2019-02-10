0e05217ce64894dd2c938098a4c13166f8846537
GBRBD <bodigaborke@gmail.com>
2019-02-10 17:36:39 +0100

    Created Header Component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from '../shared/shared.module';
 
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [SharedModule],
-  exports: []
+  exports: [HeaderComponent]
 })
 export class CoreModule {}
