84cd91c1866e6cfefd1c54ba2620b5a7bcfdae57
GBRBD <bodigaborke@gmail.com>
2019-02-11 21:27:08 +0100

    Created tests for HeaderComponent

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let headerElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    headerElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should show 'Salty' in title`, () => {
+    expect(component.title).toBe('Salty');
+  });
+
+  it('should show title in a span tag', () => {
+    const toolbarRowSpan = headerElement.querySelector('mat-toolbar-row span');
+    expect(toolbarRowSpan.innerHTML).toBe('Salty');
   });
 });
