85b43700405ff11ece5e83ecbd39c954a8b840e2
GBRBD <bodigaborke@gmail.com>
2019-02-17 21:18:45 +0100

    Update story-list.component.ts

--- src/app/story/story-list/story-list.component.ts
+++ src/app/story/story-list/story-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-story-list',
