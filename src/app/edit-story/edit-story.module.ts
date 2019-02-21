2db1f01dd76f351dc903c6ec95418e89d569c93c
GBRBD <bodigaborke@gmail.com>
2019-02-21 13:30:45 +0100

    Create EditStoryModule & EditStoryComponent

---
+++ src/app/edit-story/edit-story.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditStoryComponent } from './edit-story.component';
+
+@NgModule({
+  declarations: [EditStoryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EditStoryModule { }
